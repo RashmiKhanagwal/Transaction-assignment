9033cbc68ed28fbcf90b4818ef5f8e82b5204dcb
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-06-01 02:37:53 +0530

    database mysql configurations

---
+++ config/dbConfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: '',
+    DB: 'transactions',
+    dialect: 'mysql2',
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
