0c18019bb6d965f30dbfb8edd457687d211d3d88
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-06-01 02:53:20 +0530

    transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,18 +1,17 @@
-const { sequelize,Sequelize } = require("sequelize");
-
-const TransactionSchema = sequelize.define('Transaction',{
-    from: { 
-        type: Sequelize.STRING, 
-        allowNull: false 
-    },
-    to: { 
-        type: Sequelize.STRING, 
-        allowNull: false 
-    },
-    amount: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }
-});
-
-module.exports = TransactionSchema;
+module.exports = (sequelize,Sequelize) => {
+    const TransactionSchema = sequelize.define('Transaction',{
+        from: { 
+            type: Sequelize.STRING, 
+            allowNull: false 
+        },
+        to: { 
+            type: Sequelize.STRING, 
+            allowNull: false 
+        },
+        amount: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
+    });
+    return TransactionSchema;
+}
