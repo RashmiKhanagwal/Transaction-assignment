09c18aa7c317f96f1db30633617917a1af27e099
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-06-01 02:51:40 +0530

    user model

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
-const { sequelize,Sequelize } = require("sequelize");
-
-const UserSchema = sequelize.define('User',{
-    username: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    balance: {
-        type: Sequelize.INTEGER
-    }
-});
-
-module.exports = UserSchema;
+module.exports = (sequelize,Sequelize) => {
+    const UserSchema = sequelize.define('User',{
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        balance: {
+            type: Sequelize.INTEGER,
+            defaultValue: 1000
+        }
+    });
+    return UserSchema;
+};
