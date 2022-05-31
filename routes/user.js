56c9af65aabfc5b3387a49705e1565865807d015
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-06-01 03:22:36 +0530

    updated routes

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const router = require("express").Router();
 const userController = require("../controllers/user");
-const { verifySignUp } = require('../middleware/auth');
 
-router.post("/register", verifySignUp, userController.register);
+router.post("/register", userController.register);
 router.post("/login", userController.login);
 
 module.exports = router;
